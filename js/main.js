106a38acbb7cabd1f838b7b302d90791bf5441f1
Lorenzo Phrasavath <lorenzophrasavath@gmail.com>
2018-11-06 21:27:42 -0500

    Republish

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(() => {
+
+  /*$("#one").on("click", () => {
+    $("#img1").attr("src", "images/table_01.jpg");
+  });*/
+
+  /*$("#one").on("click", () => {
+    $("#img1").toggle();
+  });*/
+  
+  $("#two").on("click", () => {
+    $("#img2").toggle();
+  });
+  $("#three").on("click", () => {
+    $("#img3").toggle();
+  });
+  $("#four").on("click", () => {
+    $("#img4").toggle();
+  });
+
+  $("#five").on("click", () => {
+    $("#img5").toggle();
+  });
+  $("#six").on("click", () => {
+    $("#img6").toggle();
+  });
+  $("#seven").on("click", () => {
+    $("#img7").toggle();
+  });
+  $("#eight").on("click", () => {
+    $("#img8").toggle();
+  });
+
+  $("#nine").on("click", () => {
+    $("#img9").toggle();
+  });
+  $("#ten").on("click", () => {
+    $("#img10").toggle();
+  });
+  $("#eleven").on("click", () => {
+    $("#img11").toggle();
+  });
+  $("#twelve").on("click", () => {
+    $("#img12").toggle();
+  });
+
+  $("#thirteen").on("click", () => {
+    $("#img13").toggle();
+  });
+  $("#fourteen").on("click", () => {
+    $("#img14").toggle();
+  });
+  $("#fifteen").on("click", () => {
+    $("#img15").toggle();
+  });
+  $("#sixteen").on("click", () => {
+    $("#img16").toggle();
+  });
+
+});
